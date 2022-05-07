18aa7fcd019d732839bea843651af97f03904d05
hardmanhong <goandup@qq.com>
2022-05-07 11:41:03 +0800

    refactor: 重构 useToggle

---
+++ src/useToggle.ts
@@ -0,0 +1,90 @@
+import { useState, useEffect } from 'react'
+import useBoolean from './useBoolean'
+
+export type ToggleComponent = any & {
+  show: () => void
+  hide: () => void
+  toggle: (value: boolean) => void
+}
+
+type ParamsType = {
+  Component: ToggleComponent
+  onOk?: () => void
+  onCancel?: () => void
+}
+
+type useToggleReturn = [boolean, { onShow: () => void; onHide: () => void }]
+
+const useToggle = ({
+  Component,
+  onOk,
+  onCancel
+}: ParamsType): useToggleReturn => {
+  const [visible, toggle] = useBoolean()
+  const onShow = () => {
+    if (typeof onOk === 'function') {
+      onOk()
+    } else {
+      toggle(true)
+    }
+  }
+  const onHide = () => {
+    if (typeof onCancel === 'function') {
+      onCancel()
+    } else {
+      toggle(false)
+    }
+  }
+  useEffect(() => {
+    const init = () => {
+      Component.show = () => {
+        toggle(true)
+      }
+      Component.hide = () => {
+        toggle(false)
+      }
+      Component.toggle = toggle
+      return () => {
+        Component.show = () => {}
+        Component.hide = () => {}
+        Component.toggle = () => {}
+      }
+    }
+    const destroy = init()
+    return () => {
+      destroy()
+    }
+  }, [])
+  return [visible, { onShow, onHide }]
+}
+const useToggleWithPayload = <T>(
+  Component: ToggleComponent,
+  Wrapper: ToggleComponent,
+  defaultPayload: T
+): T => {
+  const [payload, setPayload] = useState(defaultPayload)
+  useEffect(() => {
+    const initInstance = () => {
+      const showCache = Component.show
+      const hideCache = Component.hide
+      const toggleCache = Component.toggle
+      Wrapper.show = (payload = defaultPayload) => {
+        setPayload(payload)
+        showCache()
+      }
+      Wrapper.hide = hideCache
+      Wrapper.toggle = toggleCache
+      return () => {
+        Wrapper.show = () => {}
+        Wrapper.hide = () => {}
+        Wrapper.toggle = () => {}
+      }
+    }
+    const destroy = initInstance()
+    return () => {
+      destroy()
+    }
+  }, [])
+  return payload
+}
+export { useToggle, useToggleWithPayload }
